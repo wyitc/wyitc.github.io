5bf50f8f77b0e1fb0bd7ce614c4f01639e5dbeb1
wyitc <wyattcole72@gmail.com>
2026-01-12 12:22:52 -0600

    add first blog post

--- js/entries.js
+++ js/entries.js
@@ -2,8 +2,8 @@ export const blog_metadata = [
     {
         "author": "author",
         "description": "Description",
-        "title": "review of Revolutionary Pekinese Opera",
-        "datetime": "1/12/2026, 12:03:42 PM",
+        "title": "rpo review",
+        "datetime": "1/12/2026, 12:21:58 PM",
         "filename": "./blogs/rpo.html"
     }
 ]
