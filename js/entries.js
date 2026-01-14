1f71c3fbde86f472b8d3f9b88fb6a5ddb0cc0bbc
wyitc <18501337+wyitc@users.noreply.github.com>
2026-01-14 21:29:57 +0000

    auto-update blog index

--- js/entries.js
+++ js/entries.js
@@ -2,6 +2,13 @@ export const blog_metadata = [
     {
         "author": "author",
         "description": "Description",
+        "title": "guess what",
+        "datetime": "1/14/2026, 1:35:49 PM",
+        "filename": "./blogs/guess what.html"
+    },
+    {
+        "author": "author",
+        "description": "Description",
         "title": "rpo review",
         "datetime": "1/12/2026, 12:21:58 PM",
         "filename": "./blogs/rpo.html"
